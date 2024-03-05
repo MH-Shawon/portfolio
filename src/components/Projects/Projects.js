d12bb294c36ee251c59a666f753d91447108baf8
Mohsin <mohsin@localhost.localdomain>
2024-03-05 23:02:38 +0600

    added all my project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import house from "../../Assets/Projects/house.svg";
+import news from "../../Assets/Projects/news.svg";
+import makeup from "../../Assets/Projects/makeup.svg";
+import factory from "../../Assets/Projects/factory.svg";
+import event from "../../Assets/Projects/event.svg";
 
 function Projects() {
   return (
@@ -23,7 +23,17 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={factory}
+              isBlog={false}
+              title="Power Tools Management"
+              description="Welcome to our power tools haven! Enjoy a seamless shopping experience with an intuitive interface, detailed product listings, and advanced search options. Shop confidently with secure transactions, personalized accounts, and responsive customer support. Discover exclusive promotions, educational content, and hassle-free returns."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              demoLink="https://powr-tools-manufacture-client.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={makeup}
               isBlog={false}
               title="Brand-Shop"
               description="Engineered a brand shop website using React, Tailwind CSS, MongoDB, and Express for an improved online experience. Elevated brand shop growth with innovative features, enhancing user satisfaction and visibility."
@@ -31,10 +41,20 @@ function Projects() {
               demoLink="https://brand-shop-assignment-bba19.web.app/"
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={house}
+              isBlog={false}
+              title="Warehouse Management"
+              description="Optimize warehouse operations with our web-based management system featuring real-time inventory tracking, intuitive order processing, and automated task scheduling. Enhance efficiency with a user-friendly interface, data analytics for informed decision-making, and seamless integration with existing systems. Ensure accuracy and speed in logistics with our comprehensive warehouse solution."
+              ghLink="https://github.com/MH-Shawon/warehouse-client"
+              demoLink="https://warehouse-client-eight.vercel.app/"
+            />
+          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={event}
               isBlog={false}
               title="Event-Management"
               description="Implemented an event-management website using React.js to deliver a dynamic and interactive userinterface, enhancing user experience for event planning and execution. Utilized Tailwind CSS for efficient andresponsive styling, ensuring a visually appealing and modern design."
@@ -45,46 +65,12 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={news}
               isBlog={false}
               title="Dragon-News"
               description="Designed and developed a news portal website featuring a responsive and user-friendly interface usingReact.js for dynamic content rendering. Incorporated Tailwind CSS for streamlined styling, ensuring anengaging and visually appealing presentation of news articles across various device"
               ghLink="https://github.com/MH-Shawon/dragon-news"
               demoLink="https://dragon-news-3d21b.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/MH-Shawon/car-doctor-server"
-              demoLink="https://car-doctor-a21c1.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
           </Col>
         </Row>
